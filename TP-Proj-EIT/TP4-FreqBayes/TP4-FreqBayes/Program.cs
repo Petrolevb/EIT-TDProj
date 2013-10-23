1af614d47d05ed8ac9b9272e067027db7b00bbc6
PetroleVB-School <Vincebellec@gmail.com>
2013-10-23 13:38:50 +0200

    TP4 with and without #hastag -> HASH

---
+++ TP-Proj-EIT/TP4-FreqBayes/TP4-FreqBayes/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace TP4_FreqBayes
+{
+    class Program
+    {
+        static void readValues(List<string> list)
+        {
+            StreamReader testStream, trainStream;
+            testStream = new StreamReader(new FileStream("twitter/test.txt", FileMode.Open, FileAccess.Read));
+            trainStream = new StreamReader(new FileStream("twitter/train.txt", FileMode.Open, FileAccess.Read));
+            while (!trainStream.EndOfStream)
+                list.Add(trainStream.ReadLine());
+            trainStream.Close();
+            testStream.Close();
+        }
+        static void Main(string[] args)
+        {
+            string prompt = "";
+            while (prompt != "QUIT")
+            {
+                #region Init Cross Validation de K, trouvé à 0.0017
+                /* Cross Validation de K
+                Dictionary<double, List<double>> resultK = new Dictionary<double, List<double>>();
+                Dictionary<double, double> moyenneK = new Dictionary<double, double>(),
+                                           ecartTypeK = new Dictionary<double, double>();
+                
+                for(int a = 450;  a < 550; a++)
+                {
+                    double exp = - a/100,
+                           k = (10 + (a/10 - (a/100)*10)) * Math.Pow(10, exp);
+                    // */ double k = 0.0017;
+                #endregion
+
+                BaseTwits baseTwits = new BaseTwits();
+                List<string> listBaseTrainTwits = new List<string>();
+                Random RNG = new Random();
+
+                readValues(listBaseTrainTwits);
+                string[] twits = listBaseTrainTwits.ToArray();
+
+                Console.Write("Taille corpus de test (" + twits.Length + " twits) (default : " + twits.Length*80/100 + ") (QUIT) : ");
+                int taille;
+                string sTaille = Console.ReadLine();
+                if (sTaille.Equals("QUIT")) { prompt = "QUIT";  continue; } // will properly exit
+
+                // default value : 80%
+                if(sTaille.Equals("")) sTaille = ((int)twits.Length*80/100).ToString();
+                if (!Int32.TryParse(sTaille, out taille))
+                { Console.WriteLine("Not a Number"); continue; }
+
+                Console.WriteLine("Press enter to run all test once, or a number to have average result");
+                prompt = Console.ReadLine();
+
+                int nombreTest;
+                if (!Int32.TryParse(prompt, out nombreTest)) nombreTest = 1;
+                List<Double> results = new List<double>();
+                while (nombreTest-- != 0)
+                {
+                    baseTwits = new BaseTwits();
+                    for (int i = 0; i < taille; i++)
+                    {
+                        int selectedNumb = RNG.Next(0, twits.Length-1-i);
+                        string selected = twits[selectedNumb];
+                        // swap with last
+                        twits[selectedNumb] = twits[twits.Length-1-i];
+                        twits[twits.Length-1-i] = selected;
+                        
+                        baseTwits.AddTwit(selected);
+                    }
+                    baseTwits.InitBase(k);
+
+                    int trueResult = 0, falseResult = 0;
+                    foreach (string t in twits)
+                    {
+                        string result = baseTwits.checkTwit(t);
+                        if (result.EndsWith("True")) trueResult++;
+                        else if (result.EndsWith("False")) falseResult++;
+                    }
+                    Console.WriteLine("True result : " + trueResult + " ~ " + (double)trueResult / (double)(trueResult + falseResult));
+                    results.Add((double)trueResult / (double)(trueResult + falseResult));
+                    /* Cross Validation de K
+                    if (!resultK.ContainsKey(k)) resultK.Add(k, new List<Double>());
+                    resultK[k].Add((double)trueResult/(double)(trueResult+falseResult));
+                    // */
+                    Console.WriteLine("False result : " + falseResult + " ~ " + (double)falseResult / (double)(trueResult + falseResult));
+                    twits = listBaseTrainTwits.ToArray();
+                    
+                }
+                double av = 0; 
+                    foreach (double var in results) av += var; 
+                    av /= results.Count;
+                double ecType = 0; 
+                    foreach (double var in results) ecType += Math.Pow(var - av, 2);
+                    ecType /= results.Count; 
+                    ecType = Math.Sqrt(ecType);
+
+                Console.WriteLine("Average : " + av + "\t" + "+/- " + ecType);
+                #region Cross Validation de K
+                /*
+                }
+                // Calc Moy & Ecart Type
+                foreach (KeyValuePair<double, List<double>> results in resultK)
+                {
+                    // Calc Moy
+                    if (!moyenneK.ContainsKey(results.Key)) moyenneK.Add(results.Key, 0);
+                    foreach (double res in results.Value)
+                        moyenneK[results.Key] += res / results.Value.Count;
+
+                    // Calc Ecart Type
+                    if (!ecartTypeK.ContainsKey(results.Key)) ecartTypeK.Add(results.Key, 0);
+                    foreach (double res in results.Value)
+                        ecartTypeK[results.Key] += Math.Pow(res - moyenneK[results.Key], 2) / results.Value.Count;
+                    ecartTypeK[results.Key] = Math.Sqrt(ecartTypeK[results.Key]);
+                }
+                // Presentation Resultats
+                // Retenu : k = 0.0017
+                foreach (KeyValuePair<double, double> ks in moyenneK)
+                    Console.WriteLine("K : " + ks.Key + "\t\t" + ks.Value + " (+/- " + ecartTypeK[ks.Key] + ")");
+                prompt = "QUIT";
+                // */
+                #endregion
+            } // fin while prompt != "QUIT"
+            Console.WriteLine("Exited..."); Console.CursorVisible = true; Console.Read();
+        }
+    }
+}
---
+++ TP-Proj-EIT/TP4-FreqBayes/TP4-FreqBayes/Twit.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TP4_FreqBayes
+{
+    class Twit
+    {
+        public enum Category { Positive, Negative, Neutral, Irrelevant, Unknown };
+
+        public Twit(string contenu)
+        {
+            string[] catType = contenu.Substring(0, contenu.IndexOf(')')).Split(',');
+            this.m_Categorie = catType[0].Remove(0, 1);
+            /* typeof(this.m_Categorie) == Enum:Category
+            string cat = catType[0].Substring(1, 1).ToUpper();
+            cat += catType[0].Substring(2);
+            this.m_Categorie = (Category)Enum.Parse(typeof(Category), cat);
+            // */
+            this.m_Type = catType[1];
+            this.m_Contenu = contenu.Remove(0, contenu.IndexOf(')')+1);
+            this.m_NbMots = new Dictionary<string, int>();
+
+            foreach (string mot in this.m_Contenu.Split(' '))
+            {
+                if (mot == null) continue;
+                if (mot == "" || mot == "\n" ||mot == "\t") continue;
+                
+                // Retrait des ponctuations
+                string newMot = mot.Replace(",","").Replace(";","").Replace(":","")
+                                    .Replace(".","").ToLower();
+                // replace #mots by HASHTAG
+                //if (newMot.StartsWith("#")) newMot = "HASHTAG";
+
+                if (!this.m_NbMots.ContainsKey(newMot))
+                    this.m_NbMots.Add(newMot, 0);
+                this.m_NbMots[newMot]++;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.m_Categorie + " - " + this.m_Type + " :: " + this.m_Contenu;
+        }
+
+        private Dictionary<String, int> m_NbMots;
+        public bool IsWordIn(string word) { return this.m_NbMots.ContainsKey(word); }
+        public int NumberWordIn(string word) { return (this.m_NbMots.ContainsKey(word) ? this.m_NbMots[word] : 0); }
+        public List<String> Words { get { return this.m_NbMots.Keys.ToList(); } }
+
+        //private Category m_Categorie;
+        //public Category Categorie { get { return this.m_Categorie; } }
+        private string m_Categorie;
+        public string Categorie { get { return this.m_Categorie; } }
+
+        private string m_Type;
+        public string Type { get { return this.m_Type; } }
+
+        private string m_Contenu;
+        public string Contenu { get { return this.m_Contenu; } }
+
+        public override bool Equals(object obj)
+        {
+            if (this == obj) return true;
+            Twit comp = obj as Twit;
+            if (comp == null) return false;
+            return this.m_Contenu.Equals(comp.Contenu);
+        }
+
+    }
+}
